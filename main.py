5d3e9cc11fff1bd87c9d8f5957f6044b40d3cc5f
sherelene <sherelenedebelen@gmail.com>
2022-04-23 09:04:55 -0700

    playing around with tkinter and goose3

--- main.py
+++ main.py
@@ -1,10 +1,63 @@
-#This is a placeholder for our code
+from tkinter import *
+from goose3 import Goose
 
 
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def info():
+    article = Goose().extract(e1.get())
+    title.set(article.title)
+    meta.set(article.meta_description)
+    string = article.cleaned_text[:150]
+    art_dec.set(string.split("\n"))
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # var for URL
+    url = "https://www.geeksforgeeks.org/python-programming-language/?ref=leftbar"
 
+    # initialization with
+    article = Goose().extract(url)
+
+    print("Title of the article :\n", article.title)
+    print("Meta information :\n", article.meta_description)
+    print("Article Text :\n", article.cleaned_text[:300])
+
+
+    # object of tkinter
+    # and background set to grey
+    master = Tk()
+    master.configure(bg='LightBlue1')
+
+    # Variable Classes in tkinter
+    title = StringVar();
+    meta = StringVar();
+    art_dec = StringVar();
+
+    # Creating label for each information
+    # name using widget Label
+    Label(master, text="Website URL : ",
+          bg="LightBlue1", font=('Arial', 12)).grid(row=0, sticky=W)
+    Label(master, text="Title :",
+          bg="LightBlue1", font=('Arial', 12)).grid(row=3, sticky=W)
+    Label(master, text="Meta information :",
+          bg="LightBlue1", font=('Arial', 12)).grid(row=4, sticky=W)
+    Label(master, text="Article description :",
+          bg="LightBlue1", font=('Arial', 12)).grid(row=5, sticky=W)
+
+    # Creating label for class variable
+    # name using widget Entry
+    Label(master, text="", textvariable=title,
+          bg="LightBlue1", font=('Arial', 11)).grid(row=3, column=1, sticky=W)
+    Label(master, text="", textvariable=meta,
+          bg="LightBlue1", font=('Arial', 11)).grid(row=4, column=1, sticky=W)
+    Label(master, text="", textvariable=art_dec,
+          bg="LightBlue1", font=('Arial', 11)).grid(row=5, column=1, sticky=W)
+
+    e1 = Entry(master, width=100)
+    e1.grid(row=0, column=1)
+
+    # creating a button using the widget
+    # to call the submit function
+    b = Button(master, text="Show", command=info, bg="light gray")
+    b.grid(row=0, column=2, columnspan=1, rowspan=1, padx=4, pady=4, )
+
+    mainloop()
