587030c99f53834e60ed4c79c45eff624491f771
sherelene <sherelenedebelen@gmail.com>
2022-04-23 13:30:05 -0700

    slowly integrating newspaper library

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from tkinter import *
 from tkinter import messagebox
 import requests
 import json
+from newspaper import Article
+import nltk
+
+# download nltk packages for tokenization
+nltk.download('punkt')
+
 
 type = 'sports'
 apiKey = 'YOUR_API_KEY_HERE'
@@ -21,6 +27,8 @@ class NewsApp:
         self.newsCat = ["general", "information",
                         "description", "article text", "video(s)", "images(s)"]
 
+
+
         # ========title frame===========#
         bg_color = "#323a45"
         text_area_bg = "#e1f3f8"
@@ -32,12 +40,18 @@ class NewsApp:
             "times new roman", 20, "bold"), bg=bg_color, fg=basic_font_color, bd=10, relief=GROOVE)
         F1.place(x=0, y=80, width=300, relheight=0.88)
 
-        for i in range(len(self.newsCat)):
-            b = Button(F1, text=self.newsCat[i].upper(
-            ), width=20, bd=7, font="arial 15 bold")
-            b.grid(row=i, column=0, padx=10, pady=5)
-            b.bind('<Button-1>', self.Newsarea)
-            self.newsCatButton.append(b)
+
+        b = Button(F1, text=self.newsCat[0].upper(
+        ), width=20, bd=7, font="arial 15 bold")
+        b.grid(row=0, column=0, padx=10, pady=5)
+        b.bind('<Button-1>', self.Newsarea)
+        self.newsCatButton.append(b)
+
+        b2 = Button(F1, text=self.newsCat[1].upper(
+        ), width=20, bd=7, font="arial 15 bold")
+        b2.grid(row=1, column=0, padx=10, pady=5)
+        b2.bind('<Button-1>', self.Newsarea)
+        self.newsCatButton.append(b2)
 
         # =======news frame=======#
         font_color = "#212121"
@@ -56,33 +70,54 @@ class NewsApp:
         self.txtarea.pack(fill=BOTH, expand=1)
 
     def Newsarea(self, event):
+        url = "https://www.opensourceforu.com/2016/02/ionic-a-ui-framework-to-simplify-hybrid-mobile-app-development/"
+
+        article = Article(url)
+
+        # 1 . Download the article
+        article.download()
+
+        # 2. Parse the article
+        article.parse()
+
+
         type = event.widget.cget('text').lower()
         BASE_URL = "https://github.com/sherelene"
         self.txtarea.delete("1.0", END)
-        self.txtarea.insert(END, f"\n Welcome to GFG news paper\n")
+        self.txtarea.insert(END, f"\n {article.authors}\n")
         self.txtarea.insert(
             END, "--------------------------------------------------------------------\n")
-        try:
-            articles = (requests.get(BASE_URL).json())['articles']
-            if (articles != 0):
-                for i in range(len(articles)):
-                    self.txtarea.insert(END, f"{articles[i]['title']}\n")
-                    self.txtarea.insert(
-                        END, f"{articles[i]['description']}\n\n")
-                    self.txtarea.insert(END, f"{articles[i]['content']}\n\n")
-                    self.txtarea.insert(
-                        END, f"read more...{articles[i]['url']}\n")
-                    self.txtarea.insert(
-                        END, "--------------------------------------------------------------------\n")
-                    self.txtarea.insert(
-                        END, "--------------------------------------------------------------------\n")
-            else:
-                self.txtarea.insert(END, "Sorry no news available")
-        except Exception as e:
-            messagebox.showerror(
-                'ERROR', "Sorry cant connect to internet or some issues with newsapp :'(")
+
+    def info(self, url):
+        article = Article(url)
+
+        # 1 . Download the article
+        article.download()
+
+        # 2. Parse the article
+        article.parse()
+
+        # 3. Fetch Author Name(s)
+        self.article.authors
+
+        # 4. Fetch Publication Date
+        print('Article Publication Date:')
+        print(article.publish_date)
+        # 5. The URL of the Major Image
+        print('Major Image in the article:')
+        print(article.top_image)
+
+        # 6. Natural Language Processing on Article to fetch Keywords
+        article.nlp()
+        print('Keywords in the article')
+        print(article.keywords)
+
+        # 7. Generate Summary of the article
+        print('Article Summary')
+        print(article.summary)
 
 
 root = Tk()
 obj = NewsApp(root)
 root.mainloop()
+
